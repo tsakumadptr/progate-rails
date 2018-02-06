d31c14c3b0d97dfc94aac58d9c08e65185e794da
tsakuma <dptr.tsakuma@gmail.com>
2018-02-06 14:32:29 +0900

    新規投稿ページ追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+
+
 end
