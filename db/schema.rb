b09882b13443b3049ea95f846a7c0145f93a565a
tsakuma <dptr.tsakuma@gmail.com>
2018-02-13 14:51:44 +0900

    ユーザー画像追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208040217) do
+ActiveRecord::Schema.define(version: 20180213053023) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180208040217) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
