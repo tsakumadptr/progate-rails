a236da7ccc1123eb7bcd007b772649ba8b309618
tsakuma <dptr.tsakuma@gmail.com>
2018-02-13 18:42:37 +0900

    ユーザーアクセス制限追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_current_user
+
+  def  set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def authenticate_user
+    if @current_user == nil
+    flash[:notice] = "You need to login"
+    redirect_to("/login")
+    end
+  end
+
+  def forbid_login_user
+    if @current_user
+      flash[:noice] = "you've been login"
+      redirect_to("/posts/index")
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user,{only:[:show,:edit,:update]}
+  before_action :forbid_login_user,{only:[:new,:create,:login_form,:login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
+
+  def ensure_correct_user
+      if @current_user.id != params[:id].to_i
+        flash[:notice] = "you don't have authority"
+        redirect_to("/posts/index")
+      end
+  end
+
 
   def index
     @users = User.all
