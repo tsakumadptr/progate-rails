b114b62052d48996f3b2c214aa22a9785cc71b13
tsakuma <dptr.tsakuma@gmail.com>
2018-02-15 11:51:18 +0900

    いいね機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,6 +62,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def likes
+    @user = User.find_by(id: params[:id])
+    @likes = Like.where(user_id: @user.id)
+  end
+
   def login_form
   end
 
